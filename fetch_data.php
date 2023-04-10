556d6b24d5e43023aaf54942c6ae4f8efaf5c74e
Martin SEBIROT <96467585+MarreTeint@users.noreply.github.com>
2023-04-10 16:40:22 +0200

    Update fetch_data.php

--- fetch_data.php
+++ fetch_data.php
@@ -18,14 +18,14 @@ $sql = "SELECT id_data, date_data, water_temp FROM Data ORDER BY id_data ASC";
 $result = $conn->query($sql);
 $waterTempData = [];
 while ($row = $result->fetch_assoc()) {
-    $waterTempData[] = ['id_data' => $row['id_data'], 'x' => $row['date_data'], 'y' => $row['water_temp']];
+    $waterTempData[] = ['id_data' => $row['id_data'], 'x' => date("Y-m-d\TH:i:s\Z",strtotime($row['date_data']), 'y' => $row['water_temp']];
 }
 
 $sql = "SELECT date_target, target_temp FROM target ORDER BY id_target DESC";
 $result = $conn->query($sql);
 $targetTempData = [];
 while ($row = $result->fetch_assoc()) {
-    $targetTempData[] = ['x' => $row['date_target'], 'y' => $row['target_temp']];
+    $targetTempData[] = ['x' => date("Y-m-d\TH:i:s\Z",strtotime($row['date_target']), 'y' => $row['target_temp']];
 }
 
 // Close the connection
