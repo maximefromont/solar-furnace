4a1b0c830d8005147cf441ba3d7cfccd3905784e
Maxime <maxime.fromont0@gmail.com>
2023-07-17 14:04:32 +0200

    Project files cleanpup

--- BDDtoHTMLScript.py
+++ BDDtoHTMLScript.py
@@ -11,27 +11,22 @@ dbname = "fsp"
 sleep_interval = 1
 
 while True:
-    # Connect to the database
     conn = pymysql.connect(host=servername, user=username, password=password, db=dbname)
 
-    # Create a cursor object
     cursor = conn.cursor()
 
     # Query to get the latest 'target_temp' value from the 'target' table
     query_target_temp = "SELECT target_temp FROM target ORDER BY id_target DESC LIMIT 1"
 
-    # Execute the query and store the result
     cursor.execute(query_target_temp)
     target_temp = int(cursor.fetchone()[0])
 
     # Query to get the latest 'water_temp' value from the 'data' table
     query_water_temp = "SELECT water_temp FROM Data ORDER BY id_data DESC LIMIT 1"
 
-    # Execute the query and store the result
     cursor.execute(query_water_temp)
     water_temp = int(cursor.fetchone()[0])
 
-    # Close the database connection
     conn.close()
 
     # Write the values to 'index.html'
@@ -40,5 +35,5 @@ while True:
 
     print(f"Values written to 'index.html': water_temp={water_temp}, target_temp={target_temp}")
 
-    # Sleep before the next iteration
+    # Sleep before the next iteration to prevent overloading the raspberry pi
     time.sleep(sleep_interval)
